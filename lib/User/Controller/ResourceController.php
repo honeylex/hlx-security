aa726548d61d7988e3e5b197288c1996c225128b
Hasham Ahmad <mr.hash@gmail.com>
2016-12-12 21:48:27 +0000

    Add authorization check to user resource

--- lib/User/Controller/ResourceController.php
+++ lib/User/Controller/ResourceController.php
@@ -4,21 +4,35 @@ namespace Hlx\Security\User\Controller;
 
 use Hlx\Security\Service\UserService;
 use Hlx\Security\User\View\ResourceSuccessView;
+use Hlx\Security\Voter\OwnershipVoter;
 use Silex\Application;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 class ResourceController
 {
     protected $userService;
 
-    public function __construct(UserService $userService)
-    {
+    protected $authorizationChecker;
+
+    public function __construct(
+        UserService $userService,
+        AuthorizationCheckerInterface $authorizationChecker
+    ) {
         $this->userService = $userService;
+        $this->authorizationChecker = $authorizationChecker;
     }
 
     public function read(Request $request, Application $app)
     {
-        $user = $this->userService->loadUserByIdentifier($request->get('userId'));
+        $userId = $request->get('userId');
+        $user = $this->userService->loadUserByIdentifier($userId);
+
+        if (!$this->authorizationChecker->isGranted([ OwnershipVoter::PERMISSION_VIEW, 'ROLE_ADMIN' ], $user)) {
+            throw new AccessDeniedException;
+        }
+
         $request->attributes->set('user', $user);
 
         return [ ResourceSuccessView::CLASS ];
